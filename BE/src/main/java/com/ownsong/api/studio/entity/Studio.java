82ac21e33cdd863d39808c16cda41c4892ff9632
changhyunKim <ch03124@naver.com>
2023-04-26 12:56:15 +0900

    feat : Studio entity 생성

---
+++ BE/src/main/java/com/ownsong/api/studio/entity/Studio.java
@@ -0,0 +1,39 @@
+package com.ownsong.api.studio.entity;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "studio")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Studio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "STUDIO_ID", columnDefinition = "INT UNSIGNED")
+    private long studioID;
+
+    @Column(name = "STUDIO_TITLE", length = 30)
+    private String studioTitle;
+
+    @Column(name = "END_DATE")
+    private LocalDateTime endDate;
+
+    @Column(name = "STUDIO_SHEET", columnDefinition = "TEXT")
+    private String studioSheet;
+
+    @Builder
+    public Studio(long studioID, String studioTitle, LocalDateTime endDate, String studioSheet) {
+        this.studioID = studioID;
+        this.studioTitle = studioTitle;
+        this.endDate = endDate;
+        this.studioSheet = studioSheet;
+    }
+}
