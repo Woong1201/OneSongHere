dc41acf76e000251d4f6adbaad2a98fa05fc7ab5
kimhero <duddnd180@naver.com>
2023-04-26 15:45:39 +0900

    feat : RelayTeam entity 생성

---
+++ BE/src/main/java/com/ownsong/api/relayStudio/entity/RelayTeam.java
@@ -0,0 +1,27 @@
+package com.ownsong.api.relayStudio.entity;
+
+
+import com.ownsong.api.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@IdClass(RelayTeamId.class)
+@Table(name = "RELAY_TEAM")
+public class RelayTeam {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RELAY_STUDIO_ID")
+    private RelayStudio relayStudio;
+
+    @Column(name = "VOTE_FLAG")
+    private Boolean voteFlag;
+}
