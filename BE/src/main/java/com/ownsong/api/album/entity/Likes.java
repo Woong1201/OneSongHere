89a4c4a8910e9a6c38204c8a37cd97a4ebd9abfe
changhyunKim <ch03124@naver.com>
2023-04-26 15:06:48 +0900

    feat : Likes entity 생성

---
+++ BE/src/main/java/com/ownsong/api/album/entity/Likes.java
@@ -0,0 +1,27 @@
+package com.ownsong.api.album.entity;
+
+import com.ownsong.api.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "LIKES")
+@NoArgsConstructor
+@IdClass(LikesId.class)
+public class Likes {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ALBUM_ID")
+    private Album album;
+
+
+}
