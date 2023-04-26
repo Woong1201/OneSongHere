72de0e56fdf16284a2c697be12c92f3096093143
changhyunKim <ch03124@naver.com>
2023-04-26 14:44:13 +0900

    feat : User 연관관계 설정

--- BE/src/main/java/com/ownsong/api/user/entity/User.java
+++ BE/src/main/java/com/ownsong/api/user/entity/User.java
@@ -1,11 +1,16 @@
 package com.ownsong.api.user.entity;
 
+import com.ownsong.api.album.entity.Album;
+import com.ownsong.api.sheet.entity.Sheet;
+import com.ownsong.api.studio.entity.Studio;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "USER")
@@ -26,6 +31,18 @@ public class User {
 
     @Column(name = "PROFILE_URL")
     private String profileUrl;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Studio> studios = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Sheet> sheets = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Album> albums = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Likes> likesList = new ArrayList<>();
 
     @Builder
     public User(long userID, String UID, String nickname, String profileUrl) {
