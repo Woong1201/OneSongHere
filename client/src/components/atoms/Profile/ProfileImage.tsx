b35782de999663a4fdea22682354c1e396802fff
임두현 <dhlim07@naver.com>
2023-04-27 15:18:36 +0900

    feat : 프로필 가로/세로 모드 전환 추가

--- client/src/components/atoms/Profile/ProfileImage.tsx
+++ client/src/components/atoms/Profile/ProfileImage.tsx
@@ -10,14 +10,22 @@ interface ProfileImageProps {
    * 이미지 크기
    */
   size?: 'small' | 'medium' | 'large';
+  /**
+   * 배치 방향
+   */
+  arrangement?: 'vertical' | 'horizontal';
 }
 
-const ProfileImage = ({ imageUrl, size = 'medium' }: ProfileImageProps) => {
+const ProfileImage = ({
+  imageUrl,
+  size = 'medium',
+  arrangement = 'vertical',
+}: ProfileImageProps) => {
   return (
     <div
       className={[
         'profile-image__container',
-        `profile-image__container--${size}`,
+        `profile-image__container--${arrangement}-${size}`,
       ].join(' ')}
     >
       <img src={imageUrl} alt="" className="profile-image__img" />
