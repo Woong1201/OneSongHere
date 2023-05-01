b37321cace584734abde89122809ee9fbfb2df63
kimhero <duddnd180@naver.com>
2023-05-01 15:30:31 +0900

    feat : Board api (GET, DELETE) 관련 추가

--- BE/src/main/java/com/ownsong/api/board/repository/BoardRepository.java
+++ BE/src/main/java/com/ownsong/api/board/repository/BoardRepository.java
@@ -2,8 +2,14 @@ package com.ownsong.api.board.repository;
 
 
 import com.ownsong.api.board.entity.Board;
+import com.ownsong.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByBoardTitleContaining(String search);
+    List<Board> findByHeader(String search);
+    List<Board> findByUser(User user);
 }
