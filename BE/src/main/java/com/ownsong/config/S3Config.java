ec42b153595f395e4f9babe93ce57c1886c2f5e1
changhyunKim <ch03124@naver.com>
2023-05-01 14:45:01 +0900

    feat : S3Config 설정

---
+++ BE/src/main/java/com/ownsong/config/S3Config.java
@@ -0,0 +1,24 @@
+package com.ownsong.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class S3Config {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+}
+
