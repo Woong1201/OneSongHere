2496dd1acc99a39f97386865e2614036c75863df
changhyunKim <ch03124@naver.com>
2023-05-01 13:11:06 +0900

    feat : 앨범전체 조회 컨트롤러 작성

--- BE/src/main/java/com/ownsong/api/album/controller/AlbumController.java
+++ BE/src/main/java/com/ownsong/api/album/controller/AlbumController.java
@@ -2,7 +2,10 @@ package com.ownsong.api.album.controller;
 
 
 import com.ownsong.api.album.dto.response.AlbumResponse;
+import com.ownsong.api.album.entity.Album;
 import com.ownsong.api.album.service.AlbumService;
+import com.ownsong.api.user.entity.User;
+import com.ownsong.api.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,29 +13,68 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Tag(name = "AlbumController", description = "앨범 API")
 @RestController
-@RequestMapping("/api/v1/album")
+@RequestMapping("/api/v1/albums")
 @RequiredArgsConstructor
 public class AlbumController {
     private final AlbumService albumService;
-
+    private final UserService userService;
+    static long userId;
     @Operation(summary = "앨범 단건 조회", description = "앨범 id로 앨범 내용 단건 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = AlbumResponse.class))),
     })
     @GetMapping(value = "{albumId}")
-    public ResponseEntity<?> getAlbums(@NotNull @PathVariable Long albumId){
-        AlbumResponse album = albumService.getAlbum(albumId);
+    public ResponseEntity<?> getAlbum(@NotNull @PathVariable Long albumId){
+        User user = userService.getLoginUser();
+        if(user == null){
+            userId = -1;
+        }else{
+            userId = user.getUserID();
+        }
+
+        AlbumResponse album = albumService.findAlbumArticle(albumId, userId);
         return ResponseEntity.ok().body(album);
+    }
+
+    @Operation(summary = "앨범 전체 조회", description = "앨범 전체 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = AlbumResponse.class))),
+    })
+    @GetMapping
+    public ResponseEntity<?> getAlbums(){
+        User user = userService.getLoginUser();
+        List<AlbumResponse> albums = albumService.getAlbumArticles();
+        return ResponseEntity.ok().body(albums);
+    }
+
+
+
+
+    @Operation(summary = "앨범 좋아요/취소", description = "앨범 좋아요/취소")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = AlbumResponse.class))),
+    })
+    @PostMapping(value = "likes/{albumId}")
+    public void updateUserLike(@NotNull @PathVariable Long albumId){
+        User user = userService.getLoginUser();
+        if(user != null){
+            userId = user.getUserID();
+            albumService.updateAlbumArticleLike(albumId, userId);
+        }
     }
 
 }
