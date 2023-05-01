260a874840076b7ca5477bc1db3d87a15571ef3e
changhyunKim <ch03124@naver.com>
2023-05-01 13:10:47 +0900

    feat : getAlbumArticles 메서드 구현

--- BE/src/main/java/com/ownsong/api/album/repository/AlbumRepositoryImpl.java
+++ BE/src/main/java/com/ownsong/api/album/repository/AlbumRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.ownsong.api.album.repository;
 
 import com.ownsong.api.album.dto.response.AlbumResponse;
+import com.ownsong.api.album.entity.Likes;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 import static com.ownsong.api.album.entity.QAlbum.album;
+import static com.ownsong.api.album.entity.QLikes.likes;
 import static com.ownsong.api.user.entity.QUser.user;
 
 @Slf4j
@@ -15,19 +19,49 @@ public class AlbumRepositoryImpl implements AlbumRepositorySupport{
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public AlbumResponse getAlbum(long albumId){
+    public AlbumResponse findAlbumArticle(long albumId){
         AlbumResponse response = queryFactory
                 .select(Projections.constructor(AlbumResponse.class,
                                 album.albumTitle,
                                 album.albumContent,
-                                album.likes,
+                                album.numberOfLikes,
                                 album.albumUrl,
                                 album.user.userID,
-                                album.user.nickname))
+                                album.user.nickname,
+                                album.albumId))
                 .from(album)
                 .where(album.albumId.eq(albumId))
                 .fetchOne();
         return response;
     }
+
+    @Override
+    public List<AlbumResponse> getAlbumArticles() {
+        List<AlbumResponse> albumArticles =queryFactory
+                .select(Projections.constructor(AlbumResponse.class,
+                        album.albumTitle,
+                        album.albumContent,
+                        album.numberOfLikes,
+                        album.albumUrl,
+                        album.user.userID,
+                        album.user.nickname,
+                        album.albumId))
+                .from(album)
+                .fetch();
+        return albumArticles;
+    }
+
+    @Override
+    public Likes findUserLike(long albumId, long userId) {
+        Likes userLike = queryFactory
+                .select(likes)
+                .from(likes)
+                .where(likes.album.albumId.eq(albumId), likes.user.userID.eq(userId))
+                .fetchOne();
+
+        log.info("------userLike : {}--------", userLike);
+        return userLike;
+    }
+
 
 }
