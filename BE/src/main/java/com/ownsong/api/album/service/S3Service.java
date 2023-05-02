2f16ef817dccff74fec544df0ae4833f8f2c71a5
changhyunKim <ch03124@naver.com>
2023-05-02 13:43:20 +0900

    feat : S3Service 구현
    파일업로드기능 구현

---
+++ BE/src/main/java/com/ownsong/api/album/service/S3Service.java
@@ -0,0 +1,60 @@
+package com.ownsong.api.album.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Transactional
+    public String uploadFile(MultipartFile file){
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        String imagePath;
+//        파일 업로드
+        try (InputStream inputStream = file.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+//            올린 오브젝트에 대한 s3 url
+            imagePath = amazonS3.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("파일 업땅");
+        }
+
+        return imagePath;
+    }
+
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        log.info("fileName : {}", fileName.substring(fileName.lastIndexOf(".")));
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+
+
+}
