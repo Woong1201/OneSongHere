49dbfaa4598e79bdcf885e8ae024264a1fe6818c
김태연 <garong0721@gmail.com>
2023-05-02 17:56:39 +0900

    feat : login page 구현

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,23 @@
-export default function Login() {
+import './Login.scss';
+import LoginButton from 'components/atoms/buttons/LoginButton';
+import google from 'assets/images/icon/google.svg';
+import kakao from 'assets/images/icon/kakao.svg';
+import naver from 'assets/images/icon/naver.svg';
+
+const Login = () => {
   return (
     <>
-      <div>로그인 내용</div>
-      <div>넣어주세요</div>
+      <div className="login-page__title">
+        <p>소셜 로그인으로 시작하기</p>
+      </div>
+
+      <div className="login-page__buttons">
+        <LoginButton type="google" src={google} label="Google로 시작하기" />
+        <LoginButton type="kakao" src={kakao} label="카카오톡으로 시작하기" />
+        <LoginButton type="naver" src={naver} label="네이버로 시작하기" />
+      </div>
     </>
   );
-}
+};
+
+export default Login;
