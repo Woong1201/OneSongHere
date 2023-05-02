f9bb2218b9e9f1188d11f58b5909e7e1ea84f13c
changhyunKim <ch03124@naver.com>
2023-05-02 13:43:34 +0900

    feat : S3Config 설정

--- BE/src/main/java/com/ownsong/config/S3Config.java
+++ BE/src/main/java/com/ownsong/config/S3Config.java
@@ -1,11 +1,17 @@
 package com.ownsong.config;
 
-import lombok.Getter;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Getter
+@RequiredArgsConstructor
 public class S3Config {
 
     @Value("${cloud.aws.s3.bucket}")
@@ -20,5 +26,14 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Bean
+    public AmazonS3 amazonS3(){
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
 }
 
