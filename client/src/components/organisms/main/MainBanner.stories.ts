0b7c540a5318d4eb2eb5b3dbecf6c87d365b357e
임두현 <dhlim07@naver.com>
2023-05-02 16:51:24 +0900

    feat : 메인 배너 molecule에서 organism으로 수정

--- client/src/components/organisms/main/MainBanner.stories.ts
+++ client/src/components/organisms/main/MainBanner.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import MainBanner from './MainBanner';
 
 const meta = {
-  title: 'molecule/mainbody/MainBanner',
+  title: 'organism/main/MainBanner',
   component: MainBanner,
   tags: ['autodocs'],
   argTypes: {
