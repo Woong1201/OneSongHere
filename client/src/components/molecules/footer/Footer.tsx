a5b299f3e5bb3a9c918414bd586101f432a22ffa
김태연 <garong0721@gmail.com>
2023-05-02 15:41:31 +0900

    chore : footer 수정

--- client/src/components/molecules/footer/Footer.tsx
+++ client/src/components/molecules/footer/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <hr />
       <div className="footer--info">
         <p>A808</p>
-        <span>
+        <p>
           대표자: 김영웅 | 개인정보보호책임자: 임두현 | 이메일:
           variety82p@gmail.com
-        </span>
+        </p>
         <p>주소: 서울특별시 강남구 역삼동 테헤란로 212 17층 1701호</p>
         <p>Copyright ©A808 All Rights Reserved.</p>
       </div>
