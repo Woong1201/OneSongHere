9be771c3661d1ee512b6aec266f5df391fdd9f54
임두현 <dhlim07@naver.com>
2023-05-03 17:56:27 +0900

    feat : 스튜디오 페이지 파일 추가

---
+++ client/src/pages/Studio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Studio.scss';
+
+const Studio = () => {
+  return (
+    <div className={['studio', 'main--full-page'].join(' ')}>
+      스튜디오 페이지
+    </div>
+  );
+};
+
+export default Studio;
