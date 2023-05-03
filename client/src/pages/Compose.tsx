1f242f1c9075d5f261ec0df22e0c91033e556eb1
김태연 <garong0721@gmail.com>
2023-05-03 17:58:15 +0900

    feat : search section 구현

--- client/src/pages/Compose.tsx
+++ client/src/pages/Compose.tsx
@@ -1,8 +1,47 @@
-export default function Compose() {
-  return (
-    <>
-      <div>작곡 페이지 내용</div>
-      <div>넣어주세요</div>
-    </>
-  );
+import { Button } from 'components/atoms/buttons/Button';
+import StudioCard from 'components/molecules/studiolist/StudioCard';
+import SearchSection from 'components/organisms/searchsection/SearchSection';
+import './Compose.scss';
+
+interface Album {
+  studioTitle: string;
+  startDate: Date;
+  endDate: Date;
+  tag: string;
 }
+const Compose = () => {
+  const date = new Date();
+  const albums: Album[] = [
+    { studioTitle: '몰라', startDate: date, endDate: date, tag: '재즈' },
+    { studioTitle: '마라', startDate: date, endDate: date, tag: '컨트리' },
+    { studioTitle: '랄라', startDate: date, endDate: date, tag: '팝' },
+    { studioTitle: '링티제로', startDate: date, endDate: date, tag: '재즈' },
+  ];
+
+  return (
+    <div className="compose-page">
+      <div className="compose-page__search-section">
+        <SearchSection />
+      </div>
+      <div className="compose-page__studio-list">
+        <p>작업중인 곡</p>
+        <Button type="button" label="생성하기" color="primary" />
+        {albums ? (
+          albums.map((album) => (
+            <StudioCard
+              key={album.studioTitle}
+              studioTitle={album.studioTitle}
+              startDate={album.startDate}
+              endDate={album.endDate}
+              tag={album.tag}
+            />
+          ))
+        ) : (
+          <p>현재 작업중인 곡이 없습니다...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Compose;
