6d5af4c8f5b30dca447f6220e0a707c855ad2041
kimhero <duddnd180@naver.com>
2023-05-03 15:48:18 +0900

    feat : RelayStudio entity 수정 총 인원수, 인원 별 마디 수, 상태 컬럼 추가

--- BE/src/main/java/com/ownsong/api/relayStudio/entity/RelayStudio.java
+++ BE/src/main/java/com/ownsong/api/relayStudio/entity/RelayStudio.java
@@ -2,6 +2,7 @@ package com.ownsong.api.relayStudio.entity;
 
 
 import com.ownsong.api.notification.entity.Notification;
+import com.ownsong.api.relayStudio.dto.request.RelayStudioCreateRequest;
 import com.ownsong.api.user.entity.User;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -34,13 +35,22 @@ public class RelayStudio {
     private String relayStudioSheet;
 
     @Column(name = "NUMBER_OF_VOTES", columnDefinition = "INT UNSIGNED")
-    private long numberOfVotes;
+    private int numberOfVotes;
 
     @Column(name = "NUMBER_OF_USERS", columnDefinition = "INT UNSIGNED")
-    private long numberOfUsers;
+    private int numberOfUsers;
 
     @Column(name = "AGREE", columnDefinition = "INT UNSIGNED")
-    private long agree;
+    private int agree;
+
+    @Column(name = "LIMIT_OF_USERS", columnDefinition = "INT UNSIGNED")
+    private int limitOfUsers;
+
+    @Column(name = "NUMBER_OF_BARS", columnDefinition = "INT UNSIGNED")
+    private int numberOfBars;
+
+    @Column(name = "STATUS", columnDefinition = "INT UNSIGNED")
+    private int status;
 
     @OneToMany(mappedBy = "relayStudio", cascade = CascadeType.ALL)
     private List<Notification> notifications = new ArrayList<>();
@@ -53,7 +63,7 @@ public class RelayStudio {
     private List<RelayTeam> relayTeams = new ArrayList<>();
 
     @Builder
-    public RelayStudio(long relayStudioID, String relayStudioTitle, LocalDateTime endDate, String relayStudioSheet, long numberOfVotes, long numberOfUsers, long agree, User user) {
+    public RelayStudio(long relayStudioID, String relayStudioTitle, LocalDateTime endDate, String relayStudioSheet, int numberOfVotes, int numberOfUsers, int agree, int limitOfUsers, int numberOfBars, int status, User user) {
         this.relayStudioID = relayStudioID;
         this.relayStudioTitle = relayStudioTitle;
         this.endDate = endDate;
@@ -61,6 +71,24 @@ public class RelayStudio {
         this.numberOfVotes = numberOfVotes;
         this.numberOfUsers = numberOfUsers;
         this.agree = agree;
+        this.limitOfUsers = limitOfUsers;
+        this.numberOfBars = numberOfBars;
+        this.status = status;
+        this.user = user;
+    }
+
+    public RelayStudio(RelayStudioCreateRequest relayStudioCreateRequest, User user) {
+        this.relayStudioTitle = relayStudioCreateRequest.getRelayStudioTitle();
+        this.relayStudioSheet = "";
+        this.limitOfUsers = relayStudioCreateRequest.getLimitOfUsers();
+        this.numberOfBars = relayStudioCreateRequest.getNumberOfBars();
+        this.numberOfUsers = 0;
+        this.status = 2;
+        this.user = user;
+    }
+
+    public void participate(User user) {
+        this.status = 2;
         this.user = user;
     }
 }
