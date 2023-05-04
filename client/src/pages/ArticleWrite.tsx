cdcef8c65b11848916cb873f13436f1e75b69a48
sin10071 <sinsh7001@gmail.com>
2023-05-04 11:13:13 +0900

    feat: 커뮤니티 글쓰기 페이지 간단히 생성 후 뒤로가기 버튼(& 함수)까지 추가

--- client/src/pages/ArticleWrite.tsx
+++ client/src/pages/ArticleWrite.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'components/atoms/buttons/Button';
+import { TextInput } from 'components/atoms/inputs/TextInput';
+
 const ArticleWrite = () => {
+  const navigate = useNavigate();
+  // 취소 클릭 시 뒤로 가도록 하는 함수
+  const goBack = () => {
+    navigate(-1);
+  };
+  // 렌더링
   return (
     <>
       <div>커뮤니티 글쓰는 페이지</div>
       <div>입니다</div>
+      <div>
+        <h1>제목</h1>
+        <TextInput label="제목을 입력해주세요" value="" stroke />
+      </div>
+      <Button label="등록" type="submit" color="primary" />
+      <Button label="취소" type="button" onClick={goBack} />
     </>
   );
 };
--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -2,15 +2,26 @@ import ArticleLine from 'components/molecules/articleline/ArticleLine';
 import SearchBar from 'components/molecules/searchsection/SearchBar';
 import { Button } from 'components/atoms/buttons/Button';
 import TextButton from 'components/atoms/buttons/TextButton';
+import { useNavigate } from 'react-router-dom';
 import './Board.scss';
 
 const Board = () => {
+  const navigate = useNavigate();
+  const navigateWritePage = () => {
+    navigate('/board/write');
+  };
+
   return (
     <div className="board__container">
       <div>커뮤니티 전체 페이지</div>
       <div>입니다</div>
       <SearchBar label="검색어를 입력하세요" />
-      <Button label="글쓰기" type="submit" color="primary" />
+      <Button
+        label="글쓰기"
+        type="submit"
+        color="primary"
+        onClick={navigateWritePage}
+      />
       <div className="category__container">
         {/* onclick으로 카테고리에 맞게 아티클들 필터링할 수 있게 해줘야 함 */}
         <TextButton label="구인" />
