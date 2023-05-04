5efb7af04f0ad812bff855b7461a492cffeac294
changhyunKim <ch03124@naver.com>
2023-05-04 16:36:50 +0900

    feat : 스튜디오 정보 조회 인터페이스 메소드 추가

--- BE/src/main/java/com/ownsong/api/studio/repository/StudioRepositorySupport.java
+++ BE/src/main/java/com/ownsong/api/studio/repository/StudioRepositorySupport.java
@@ -1,10 +1,12 @@
 package com.ownsong.api.studio.repository;
 
-import com.ownsong.api.studio.dto.responese.StudioCreateResponse;
+import com.ownsong.api.studio.dto.responese.StudioEntranceResponse;
+import com.ownsong.api.studio.dto.responese.StudioResponse;
 import com.ownsong.api.user.entity.User;
 
 import java.util.List;
 
 public interface StudioRepositorySupport {
-    List<StudioCreateResponse> getParticipatedStudios(User user);
+    List<StudioResponse> getParticipatedStudios(User user);
+    StudioEntranceResponse getParticipatedStudioInfo(long studioId);
 }
