5d3d80766dc4dbbca0330afb68ee9cf624ccdc39
sin10071 <sinsh7001@gmail.com>
2023-05-04 17:38:27 +0900

    feat: ArticleHeader 수정 중

--- client/src/components/molecules/articleheader/ArticleHeader.tsx
+++ client/src/components/molecules/articleheader/ArticleHeader.tsx
@@ -6,12 +6,19 @@ import Profile from 'components/molecules/Profilesection/Profile';
 // {}: ArticleHeaderProps
 const ArticleHeader = () => {
   return (
-    <Profile
-      imageUrl="https://images.squarespace-cdn.com/content/v1/62cd860e06ceca2438b4b307/6dfdcd3e-e6a1-479d-b393-dfa790cacd4a/Badger+Ham+Smoked+Honey+Ham"
-      nickName="ham"
-      size="small"
-      arrangement="horizontal"
-    />
+    <div>
+      <div>카테고리</div>
+      <div>너만 오면 고</div>
+      <Profile
+        imageUrl="https://images.squarespace-cdn.com/content/v1/62cd860e06ceca2438b4b307/6dfdcd3e-e6a1-479d-b393-dfa790cacd4a/Badger+Ham+Smoked+Honey+Ham"
+        nickName="ham"
+        size="small"
+        arrangement="horizontal"
+      />
+      <div>2023.05.04</div>
+      <div>12</div>
+      <div>2</div>
+    </div>
   );
 };
 
