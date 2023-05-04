998a48869185f10c0988130eb53834ea333ac579
kimhero <duddnd180@naver.com>
2023-05-04 12:56:55 +0900

    feat : compose 기능 및 delete 예시 method 추가

--- BE/src/main/java/com/ownsong/api/relayStudio/service/RelayStudioService.java
+++ BE/src/main/java/com/ownsong/api/relayStudio/service/RelayStudioService.java
@@ -1,15 +1,16 @@
 package com.ownsong.api.relayStudio.service;
 
+import com.ownsong.api.relayStudio.dto.request.RelayStudioComposeRequest;
 import com.ownsong.api.relayStudio.entity.RelayStudio;
 import com.ownsong.api.relayStudio.entity.RelayTeam;
 import com.ownsong.api.relayStudio.repository.RelayStudioRepository;
 import com.ownsong.api.relayStudio.dto.request.RelayStudioCreateRequest;
 import com.ownsong.api.relayStudio.dto.response.RelayStudioResponse;
+import com.ownsong.api.relayStudio.repository.RelayTeamRepository;
 import com.ownsong.api.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 
 @Slf4j
@@ -18,9 +19,11 @@ import javax.transaction.Transactional;
 @Transactional
 public class RelayStudioService {
     private final RelayStudioRepository relayStudioRepository;
+    private final RelayTeamRepository relayTeamRepository;
+
     public RelayStudioResponse createRelayStudio(RelayStudioCreateRequest relayStudioCreateRequest, User user) {
         RelayStudio relayStudio = relayStudioRepository.save(new RelayStudio(relayStudioCreateRequest, user));
-        return new RelayStudioResponse(relayStudio, false);
+        return new RelayStudioResponse(relayStudio, false, false);
     }
 
     public RelayStudioResponse participateRelayStudio(Long relayStudioId, User user) {
@@ -45,6 +48,59 @@ public class RelayStudioService {
         // relayStudio 업데이트 후 return
         relayStudio.participate(user);
         relayStudioRepository.save(relayStudio);
-        return new RelayStudioResponse(relayStudio, false);
+        return new RelayStudioResponse(relayStudio, false, false);
+    }
+
+    public RelayStudioResponse composeRelayStudio(RelayStudioComposeRequest relayStudioComposeRequest, User user) {
+        RelayStudio relayStudio;
+        // 해당 relayStudio 존재 여부 확인
+        try {
+            relayStudio = relayStudioRepository.findById(relayStudioComposeRequest.getRelayStudioID()).get();
+        }catch (Exception e) {
+            return null;
+        }
+
+        // 해당 relayStudio 에 접근 가능한지 확인
+        if (relayStudio.getStatus() != 2 || relayStudio.getUser().getUserID() != user.getUserID())
+            return null;
+
+        // 릴레이 작곡 시작 유저인 경우
+        if (relayStudio.getNumberOfUsers() == 0) {
+            relayStudio.leader(relayStudioComposeRequest);
+            RelayTeam relayTeam = new RelayTeam(relayStudio);
+            relayStudio.getRelayTeams().add(relayTeam);
+            relayStudioRepository.save(relayStudio);
+            return new RelayStudioResponse(relayStudio, true, false);
+        }
+
+        // relayStudio 업데이트 및 투표 초기화 후 return (추후 알림 필요)
+        relayStudio.update(relayStudioComposeRequest);
+        relayStudioRepository.save(relayStudio);
+        return new RelayStudioResponse(relayStudio, false, false);
+    }
+
+    public boolean deleteTeam(Long relayStudioId, User user) {
+        RelayStudio relayStudio;
+        // 해당 relayStudio 존재 여부 확인
+        try {
+            relayStudio = relayStudioRepository.findById(relayStudioId).get();
+        }catch (Exception e) {
+            return false;
+        }
+
+        // 입력한 relayStudio 와 login 된 user 로 이루어진 relayTeam 확인 후 삭제
+        try {
+            for (RelayTeam relayTeam : relayStudio.getRelayTeams()) {
+                if (relayTeam.getUser().getUserID() == user.getUserID()) {
+                    // 부모 entity 에서 연결을 끊어준 후 삭제
+                    relayStudio.getRelayTeams().remove(relayTeam);
+                    relayTeamRepository.deleteRelayTeamByUserAndRelayStudio(user, relayStudio);
+                    return true;
+                }
+            }
+        }catch (Exception e) {
+            return false;
+        }
+        return false;
     }
 }
