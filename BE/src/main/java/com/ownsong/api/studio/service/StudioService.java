d27bd26dbd4bba8c66dd2464b9ac8fe30d98f0eb
changhyunKim <ch03124@naver.com>
2023-05-04 15:38:23 +0900

    feat : 스튜디오 생성 로직 구현(StudioService)

--- BE/src/main/java/com/ownsong/api/studio/service/StudioService.java
+++ BE/src/main/java/com/ownsong/api/studio/service/StudioService.java
@@ -1,10 +1,9 @@
 package com.ownsong.api.studio.service;
 
 
-import com.ownsong.api.studio.dto.responese.StudioCreateResponse;
+import com.ownsong.api.studio.dto.request.StudioCreateRequest;
+import com.ownsong.api.studio.dto.responese.StudioResponse;
 import com.ownsong.api.studio.entity.Studio;
-import com.ownsong.api.studio.entity.StudioTeam;
-import com.ownsong.api.studio.entity.StudioTeamId;
 import com.ownsong.api.studio.repository.StudioRepository;
 import com.ownsong.api.studio.repository.StudioTeamRepository;
 import com.ownsong.api.user.entity.User;
@@ -13,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -23,9 +23,31 @@ public class StudioService {
     private final StudioRepository studioRepository;
     private final StudioTeamRepository studioTeamRepository;
 
-    public List<StudioCreateResponse> getParticipatedStudios(User user){
-        List<StudioCreateResponse> studios = studioRepository.getParticipatedStudios(user);
+    public List<StudioResponse> getParticipatedStudios(User user){
+        List<StudioResponse> studios = studioRepository.getParticipatedStudios(user);
 
         return studios;
+    }
+
+    @Transactional
+    public StudioResponse createStudio(StudioCreateRequest studioCreateRequest, User user){
+        LocalDateTime endDate = LocalDateTime.now().plusDays(7);
+
+        Studio studio = Studio.builder()
+                .studioTitle(studioCreateRequest.getStudioTitle())
+                .user(user)
+                .genre(studioCreateRequest.getGenre())
+                .endDate(endDate)
+                .build();
+
+        StudioResponse studioResponse = StudioResponse.builder()
+                .studioTitle(studioCreateRequest.getStudioTitle())
+                .hostId(user.getNickname())
+                .genre(studioCreateRequest.getGenre())
+                .endDate(endDate)
+                .build();
+
+        studioRepository.save(studio);
+        return studioResponse;
     }
 }
