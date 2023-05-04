0f38402aee0299cbb8fda5c348edea93c8d06c9c
changhyunKim <ch03124@naver.com>
2023-05-04 15:39:42 +0900

    refactor : StudioResponse 생성자 수정

--- BE/src/main/java/com/ownsong/api/studio/dto/responese/StudioResponse.java
+++ BE/src/main/java/com/ownsong/api/studio/dto/responese/StudioResponse.java
@@ -25,20 +25,19 @@ public class StudioResponse {
     @NotNull
     private String genre;
 
-    @Schema(description = "호스트(방장) id", example = "3")
-    @NotNull
-    private long hostId;
-
     @Schema(description = "스튜디오 종료일자", example = "2023-05-04T04:38:20.871Z")
     @NotNull
     private LocalDateTime endDate;
+    @Schema(description = "호스트(방장) id", example = "3")
+    private String hostId;
+
 
     @Builder
-    public StudioResponse(long studioId, String studioTitle, String genre, long hostId, LocalDateTime endDate) {
+    public StudioResponse(long studioId, String studioTitle, String genre, LocalDateTime endDate, String hostId) {
         this.studioId = studioId;
         this.studioTitle = studioTitle;
         this.genre = genre;
-        this.hostId = hostId;
         this.endDate = endDate;
+        this.hostId = hostId;
     }
 }
