cdc1cb555763116bab9dca191735890af940ddaa
sin10071 <sinsh7001@gmail.com>
2023-05-04 15:17:17 +0900

    feat: Board에 table 생성

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -5,6 +5,18 @@ import TextButton from 'components/atoms/buttons/TextButton';
 import { useNavigate } from 'react-router-dom';
 import './Board.scss';
 
+interface Article {
+  id: number;
+  title: string;
+  writer: string;
+  date: string;
+  viewCnt: number;
+}
+const articles: Article[] = [
+  { id: 1, title: 'one', writer: 'oneone', date: '2023.05.04', viewCnt: 3 },
+  { id: 2, title: 'two', writer: 'twotwo', date: '2023.05.04', viewCnt: 4 },
+];
+
 const Board = () => {
   const navigate = useNavigate();
   const navigateWritePage = () => {
@@ -12,16 +24,7 @@ const Board = () => {
   };
 
   return (
-    <div className="board__container">
-      <div>커뮤니티 전체 페이지</div>
-      <div>입니다</div>
-      <SearchBar />
-      <Button
-        label="글쓰기"
-        type="submit"
-        color="primary"
-        onClick={navigateWritePage}
-      />
+    <div>
       <div className="category__container">
         {/* onclick으로 카테고리에 맞게 아티클들 필터링할 수 있게 해줘야 함 */}
         <TextButton label="구인" />
@@ -29,13 +32,54 @@ const Board = () => {
         <TextButton label="홍보" />
         <TextButton label="잡담" />
       </div>
-      <ArticleLine
-        num={1}
-        title="what"
-        commentCnt={2}
-        writer="are"
-        birthday="you doing"
-      />
+      <div className="board__container">
+        <div>커뮤니티 전체 페이지</div>
+        <div>입니다</div>
+        <SearchBar />
+        <Button
+          label="글쓰기"
+          type="submit"
+          color="primary"
+          onClick={navigateWritePage}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>날짜</th>
+              <th>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((article) => (
+              <tr key={article.id}>
+                <td>{article.id}</td>
+                <td>{article.title}</td>
+                <td>{article.writer}</td>
+                <td>{article.date}</td>
+                <td>{article.viewCnt}</td>
+                <ArticleLine
+                  num={article.id}
+                  title={article.title}
+                  commentCnt={article.viewCnt}
+                  writer={article.writer}
+                  birthday={article.date}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <ArticleLine
+          num={1}
+          title="what"
+          commentCnt={2}
+          writer="are"
+          birthday="you doing"
+        />
+      </div>
     </div>
   );
 };
