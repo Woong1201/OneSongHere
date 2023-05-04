c4f203ebb3fb64b5a6d9d0368ca412978c7cd332
임두현 <dhlim07@naver.com>
2023-05-04 14:48:01 +0900

    feat : 프로필 리스트 molecule 제작

---
+++ client/src/components/molecules/studioheader/ProfileImageList.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ProfileImageList.scss';
+import ProfileImage from 'components/atoms/profile/ProfileImage';
+import User from 'types/User';
+
+interface ProfileImageListProps {
+  /**
+   * 유저 객체 리스트
+   */
+  users: User[];
+}
+
+const ProfileImageList = ({ users }: ProfileImageListProps) => {
+  return (
+    <div className="studio__header-profile">
+      {users.map((user) => (
+        <ProfileImage
+          key={user.userId}
+          size="medium"
+          arrangement="horizontal"
+          imageUrl={user.picture}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProfileImageList;
