1e761a426b077085b9a44157f8eced55ac6d4e19
임두현 <dhlim07@naver.com>
2023-05-08 10:18:14 +0900

    chore : 타입 변경에 따른 accessToken 더미 데이터 추가

--- client/src/components/organisms/common/studio/StudioHeader.tsx
+++ client/src/components/organisms/common/studio/StudioHeader.tsx
@@ -9,18 +9,21 @@ const StudioHeader = () => {
   const users = [
     {
       userId: 1,
+      accessToken: '111',
       nickname: '신선호',
       picture:
         'https://file.mk.co.kr/mkde/N0/2016/03/201603080305561821779.jpg',
     },
     {
       userId: 2,
+      accessToken: '111',
       nickname: '김태연',
       picture:
         'https://file.mk.co.kr/meet/neds/2023/03/image_readtop_2023_195678_16786077015385435.jpg',
     },
     {
       userId: 3,
+      accessToken: '111',
       nickname: '김영웅',
       picture: 'https://slamdunk-movie.jp/files/images/p_main_akagi.jpg',
     },
