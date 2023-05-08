bdf409ab0c5f43ce32e2250b090fe62a96b2e5f4
임두현 <dhlim07@naver.com>
2023-05-08 17:48:55 +0900

    feat : 프로필 드랍다운 아이템 atom 제작

---
+++ client/src/components/atoms/profiledropdown/ProfileDropdownItem.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ProfileDropdownItem from './ProfileDropdownItem';
+
+const meta = {
+  title: 'atom/ProfileDropdown/ProfileDropdownItem',
+  component: ProfileDropdownItem,
+  tags: ['autodocs'],
+  argTypes: {
+    //
+  },
+} satisfies Meta<typeof ProfileDropdownItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: { label: '내 프로필' },
+};
