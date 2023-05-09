72509515bf71fd4c39579f0d8b851231d2359c54
kimhero <duddnd180@naver.com>
2023-05-09 13:48:54 +0900

    feat : UserRepository 수정 email 을 통해 user 조회

--- BE/src/main/java/com/ownsong/api/user/repository/UserRepository.java
+++ BE/src/main/java/com/ownsong/api/user/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUID(String UID);
     List<User> findByNicknameContaining(String search);
+    User findByEmail(String email);
 }
