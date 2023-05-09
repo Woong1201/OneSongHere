6308659061a596275b95bb90ee0829263e7bdfda
sin10071 <sinsh7001@gmail.com>
2023-05-09 13:27:23 +0900

    feat: 게시글 및 댓글 정보 api로 가져오도록 함

--- client/src/pages/Article.tsx
+++ client/src/pages/Article.tsx
@@ -1,9 +1,59 @@
+import React, { useState, useEffect } from 'react';
 import ArticleHeader from 'components/molecules/articleheader/ArticleHeader';
+import { getArticle } from 'services/board';
+import { useParams } from 'react-router-dom';
+
+interface CommentResponse {
+  commentId: number;
+  userId: number;
+  nickName: string;
+  commentContent: string;
+  commentDate: string;
+}
+
+interface BoardResponse {
+  boardId: number;
+  userId: number;
+  nickName: string;
+  boardTitle: string;
+  header: string;
+  boardContent: string;
+  boardDate: string;
+  commentResponses: CommentResponse[];
+}
 
 const Article = () => {
+  const boardId = useParams();
+  const [articleInfo, getArticleInfo] = useState<BoardResponse>();
+  useEffect(() => {
+    getArticle(
+      Number(boardId.articleId),
+      ({ data }) => {
+        console.log('data :', data);
+        getArticleInfo(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
   return (
     <>
       <ArticleHeader />
+      {articleInfo?.boardContent}
+      {articleInfo?.boardDate}
+      {articleInfo?.boardId}
+      {articleInfo?.boardTitle}
+      {articleInfo?.header}
+      {articleInfo?.nickName}
+      {articleInfo?.userId}
+      {articleInfo?.commentResponses.map((comment) => (
+        <div key={comment.commentId}>
+          <div>{comment.nickName}</div>
+          <div>{comment.commentContent}</div>
+          <div>{comment.commentDate}</div>
+        </div>
+      ))}
       <div>게시글 헤더 molecules</div>
       <div>제목</div>
       <div>프로필아이콘</div>
