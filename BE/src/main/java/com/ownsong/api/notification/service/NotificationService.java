7e4c1ec117eb86168125a20ada38358b1d3d3898
kimhero <duddnd180@naver.com>
2023-05-10 15:17:10 +0900

    feat : NotificationService notification 조회 기능 추가

--- BE/src/main/java/com/ownsong/api/notification/service/NotificationService.java
+++ BE/src/main/java/com/ownsong/api/notification/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.ownsong.api.notification.service;
 
+import com.ownsong.api.notification.dto.response.NotificationResponse;
 import com.ownsong.api.notification.entity.Notification;
 import com.ownsong.api.notification.repository.NotificationRepository;
 import com.ownsong.api.user.entity.User;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -29,5 +32,19 @@ public class NotificationService {
             return false;
         notificationRepository.delete(notification);
         return true;
+    }
+
+    public List<NotificationResponse> getNotifications(User user) {
+        List<Notification> notifications;
+        try {
+            notifications = notificationRepository.findAllByUser(user);
+        } catch (Exception e) {
+            return null;
+        }
+        List<NotificationResponse> notificationResponses = new ArrayList<>();
+        for (Notification notification : notifications) {
+            notificationResponses.add(new NotificationResponse(notification));
+        }
+        return notificationResponses;
     }
 }
