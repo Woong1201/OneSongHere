a58f895352fd37651988d5d3ab5569f048fbf398
kimhero <duddnd180@naver.com>
2023-05-10 15:16:36 +0900

    feat : NotificationRepository user 에 대한 조회 추가

--- BE/src/main/java/com/ownsong/api/notification/repository/NotificationRepository.java
+++ BE/src/main/java/com/ownsong/api/notification/repository/NotificationRepository.java
@@ -2,8 +2,12 @@ package com.ownsong.api.notification.repository;
 
 
 import com.ownsong.api.notification.entity.Notification;
+import com.ownsong.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+    List<Notification> findAllByUser(User user);
 }
