cab3fac0a31ce3469a5b3701c4211f8e0d023753
김태연 <garong0721@gmail.com>
2023-05-10 13:42:41 +0900

    feat : 프로필 dropdown item에 icon 추가

--- client/src/components/atoms/profiledropdown/ProfileDropdownItem.tsx
+++ client/src/components/atoms/profiledropdown/ProfileDropdownItem.tsx
@@ -3,12 +3,18 @@ import './ProfileDropdownItem.scss';
 
 interface ProfileDropdownItemProps {
   label: string;
+  icon: React.ReactElement | string;
   onClick?: () => void;
 }
 
-const ProfileDropdownItem = ({ label, onClick }: ProfileDropdownItemProps) => {
+const ProfileDropdownItem = ({
+  label,
+  icon,
+  onClick,
+}: ProfileDropdownItemProps) => {
   return (
     <button type="button" className="profile-dropdown__item" onClick={onClick}>
+      {icon}
       {label}
     </button>
   );
