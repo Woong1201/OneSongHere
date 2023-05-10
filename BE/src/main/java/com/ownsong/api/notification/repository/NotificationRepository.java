059787c807702e6e434772941d637130cb1d364c
kimhero <duddnd180@naver.com>
2023-05-10 17:53:33 +0900

    feat : NotificationRepository delete 추가 RelayStudio 에 대한 추가

--- BE/src/main/java/com/ownsong/api/notification/repository/NotificationRepository.java
+++ BE/src/main/java/com/ownsong/api/notification/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.ownsong.api.notification.repository;
 
 
 import com.ownsong.api.notification.entity.Notification;
+import com.ownsong.api.relayStudio.entity.RelayStudio;
 import com.ownsong.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,5 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findAllByUser(User user);
+    void deleteAllByRelayStudio(RelayStudio relayStudio);
 }
