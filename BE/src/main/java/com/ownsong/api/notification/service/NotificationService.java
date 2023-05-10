05d406d45b07b04110c64212ff573f04dbcee25a
kimhero <duddnd180@naver.com>
2023-05-10 11:06:06 +0900

    feat : NotificationService 생성

---
+++ BE/src/main/java/com/ownsong/api/notification/service/NotificationService.java
@@ -0,0 +1,15 @@
+package com.ownsong.api.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class NotificationService {
+
+}
