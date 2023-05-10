fc0ba257e0a4a55cc748184fd64fbc18202fdc6d
kimhero <duddnd180@naver.com>
2023-05-10 14:27:50 +0900

    feat : NotificationService delete 추가

--- BE/src/main/java/com/ownsong/api/notification/service/NotificationService.java
+++ BE/src/main/java/com/ownsong/api/notification/service/NotificationService.java
@@ -1,5 +1,8 @@
 package com.ownsong.api.notification.service;
 
+import com.ownsong.api.notification.entity.Notification;
+import com.ownsong.api.notification.repository.NotificationRepository;
+import com.ownsong.api.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,20 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class NotificationService {
+    private final NotificationRepository notificationRepository;
 
+    public boolean deleteNotification(Long notiId, User user) {
+        Notification notification;
+        try {
+            notification = notificationRepository.findById(notiId).get();
+            if (notification == null)
+                return false;
+        } catch (Exception e) {
+            return false;
+        }
+        if (notification.getUser().getUserID() != user.getUserID())
+            return false;
+        notificationRepository.delete(notification);
+        return true;
+    }
 }
