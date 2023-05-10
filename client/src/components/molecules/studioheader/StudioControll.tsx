8874c04bdefca8a076b81926a116bf0b16a39b40
임두현 <dhlim07@naver.com>
2023-05-10 17:26:37 +0900

    feat : 노트 재생 기능 추가

--- client/src/components/molecules/studioheader/StudioControll.tsx
+++ client/src/components/molecules/studioheader/StudioControll.tsx
@@ -2,18 +2,62 @@ import React from 'react';
 import './StudioControll.scss';
 import LogoIcon from 'components/atoms/common/LogoIcon';
 import PlayIcon from 'components/atoms/stuidioHeader/PlayIcon';
+
 import StopIcon from 'components/atoms/stuidioHeader/StopIcon';
 
-const StudioControll = () => {
+import * as Tone from 'tone';
+import Note from 'types/Note';
+
+interface StudioControllProps {
+  notes: Note[];
+}
+
+const StudioControll = ({ notes }: StudioControllProps) => {
+  // 시퀀스 재생 메소드
+  const playSequence = () => {
+    // Tone의 Sampler로 피아노 파일 인스턴스를 만듭니다
+    const piano = new Tone.Sampler({
+      urls: {
+        C4: 'C4.mp3',
+        'D#4': 'Ds4.mp3',
+        'F#4': 'Fs4.mp3',
+        A4: 'A4.mp3',
+      },
+      release: 1,
+      baseUrl: 'https://tonejs.github.io/audio/salamander/',
+    }).toDestination();
+    // Tone이 로드된 후
+    Tone.loaded().then(() => {
+      // Notes 데이터에 각 노트들
+      notes.forEach((note) => {
+        const now = Tone.now();
+        // triggerAttackRelease로 재생해줍니다 노트, 지속시간, 타이밍
+        piano.triggerAttackRelease(note.note, note.duration, now + note.timing);
+      });
+    });
+  };
+
+  const stopSequence = () => {
+    Tone.Transport.stop();
+  };
+
   return (
     <div className="studio__header-controll">
       <LogoIcon goHome size="small" whiteMode />
-      <div className="studio__header-controll-icon">
+      <button
+        type="button"
+        onClick={playSequence}
+        className="studio__header-controll-icon"
+      >
         <PlayIcon size={30} />
-      </div>
-      <div className="studio__header-controll-icon">
+      </button>
+      {/* <button
+        type="button"
+        onClick={stopSequence}
+        className="studio__header-controll-icon"
+      >
         <StopIcon size={30} />
-      </div>
+      </button> */}
     </div>
   );
 };
