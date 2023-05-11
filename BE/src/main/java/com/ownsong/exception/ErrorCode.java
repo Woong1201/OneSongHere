a96b47d6c0a6cc856c208b757bfe2720e9f9f50e
changhyunKim <ch03124@naver.com>
2023-05-11 14:38:41 +0900

    feat : ErrorCode 추가
    USER_UNAUTHORIZED 추가

--- BE/src/main/java/com/ownsong/exception/ErrorCode.java
+++ BE/src/main/java/com/ownsong/exception/ErrorCode.java
@@ -5,11 +5,14 @@ public enum ErrorCode {
 
     // Global
     INVALID_INPUT_VALUE(400, "G001", "Invalid Input Value"),
-    ACCESS_Unauthorized(401, "G002", "유효한 인증 정보가 아닙니다"),
-    ACCESS_Forbidden(403, "G003", "해당 권한이 없습니다."),
+    ACCESS_UNAUTHORIZED(401, "G002", "유효한 인증 정보가 아닙니다"),
+    ACCESS_FORBIDDEN(403, "G003", "해당 권한이 없습니다."),
     PAGE_NOT_FOUND(404, "G004", "잘못된 경로입니다."),
     METHOD_NOT_ALLOWED(405, "G005", "Invalid Input Value"),
-    INTERNAL_SERVER_ERROR(500, "G006", "Internal Server Error");
+    INTERNAL_SERVER_ERROR(500, "G006", "Internal Server Error"),
+
+    //USER
+    USER_UNAUTHORIZED(401, "U001", "로그인이 유효하지 않습니다.");
 
     private final String code;
     private final String message;
