fd7bad55f5ddebae7c71e2b213ea5fac9f270a05
kimhero <duddnd180@naver.com>
2023-05-14 01:25:16 +0900

    feat : AlbumResponse dto 수정 tag 추가

--- BE/src/main/java/com/ownsong/api/album/dto/response/AlbumResponse.java
+++ BE/src/main/java/com/ownsong/api/album/dto/response/AlbumResponse.java
@@ -1,5 +1,7 @@
 package com.ownsong.api.album.dto.response;
 
+import com.ownsong.api.album.entity.Album;
+import com.ownsong.api.album.entity.AlbumTag;
 import com.ownsong.api.album.entity.Likes;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -7,16 +9,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class AlbumResponse {
 
-    @Schema(description = "앨범제목", example = "영웅이의 2집 앨범")
+    @Schema(description = "앨범제목", example = "창현이의 2집 앨범")
     @NotNull
     private String albumTitle;
 
-    @Schema(description = "앨범 내용", example = "영웅이의 화난 마음을 담은 힙합노래")
+    @Schema(description = "앨범 내용", example = "창현이의 화난 마음을 담은 힙합노래")
     @NotNull
     private String albumContent;
 
@@ -32,7 +36,7 @@ public class AlbumResponse {
     @NotNull
     private long userId;
 
-    @Schema(description = "유저 닉네임", example = "zi존i히i어i로iz")
+    @Schema(description = "유저 닉네임", example = "zi존i창1현1iz")
     @NotNull
     private String nickName;
 
@@ -40,16 +44,17 @@ public class AlbumResponse {
     @NotNull
     private boolean userLike = false;
 
-    @Schema(description = "장르", example = "힙합")
-    private String genre;
-
-    @Schema(description = "앨범 mp3 s3 주소", example = "https://s3어쩌구저쩌구")
+    @Schema(description = "앨범 mp3 s3 주소", example = "https://s3, 현재는 albumSheet")
     private String mp3Url;
 
+    @Schema(description = "album_id(DB의 PK로서의 )", example = "412")
     private long albumId;
 
+    @Schema(description = "tags", example = "['힙합', '발라드']")
+    private List<String> tags = new ArrayList<>();
+
     @Builder
-    public AlbumResponse(String albumTitle, String albumContent, long likes, String albumUrl, long userId, String nickName, long albumId, String genre, String mp3Url) {
+    public AlbumResponse(String albumTitle, String albumContent, long likes, String albumUrl, long userId, String nickName, long albumId, String mp3Url) {
         this.albumTitle = albumTitle;
         this.albumContent = albumContent;
         this.likes = likes;
@@ -57,10 +62,22 @@ public class AlbumResponse {
         this.userId = userId;
         this.nickName = nickName;
         this.albumId = albumId;
-        this.genre = genre;
         this.mp3Url = mp3Url;
     }
 
+    public AlbumResponse(Album album) {
+        this.albumTitle = album.getAlbumTitle();
+        this.albumContent = album.getAlbumContent();
+        this.likes = album.getNumberOfLikes();
+        this.albumUrl = album.getAlbumUrl();
+        this.userId = album.getUser().getUserID();
+        this.nickName = album.getUser().getNickname();
+        this.albumId = album.getAlbumId();
+        this.mp3Url = album.getMp3Url();
+        for (AlbumTag albumTag : album.getAlbumTags()) {
+            this.tags.add(albumTag.getAlbumTagContent());
+        }
+    }
 
     public void setUserLike(Likes userLike){
         if(userLike != null){
