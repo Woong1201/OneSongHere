52ef309256a39c846fa3888f187f479c0cef54f6
임두현 <dhlim07@naver.com>
2023-05-15 17:57:52 +0900

    feat : 코드 섹션 제작

--- client/src/components/organisms/studio/StudioChord.tsx
+++ client/src/components/organisms/studio/StudioChord.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './StudioChord.scss';
+import { Note } from 'types/Note';
+import { Chord } from 'types/Chord';
+import StudioChordCard from 'components/molecules/studiochord/StudioChordCard';
+import StudioChordCardList from 'components/molecules/studiochord/StudioChordCardList';
+import SectionTitle from 'components/atoms/common/SectionTitle';
 
-const StudioChord = () => {
-  return <div></div>;
+interface StudioChordProps {
+  chordNotes: Record<Chord, Note>;
+  updateChord: (chord: Chord) => void;
+}
+
+const StudioChord = ({ chordNotes, updateChord }: StudioChordProps) => {
+  return (
+    <div className="studio__chord">
+      <div className="studio__chord-title-area">
+        <SectionTitle title="코드 입력" />
+      </div>
+      <StudioChordCardList chordNotes={chordNotes} updateChord={updateChord} />
+    </div>
+  );
 };
 
 export default StudioChord;
